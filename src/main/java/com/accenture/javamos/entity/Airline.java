2263c1d8c2842136bbdf1c974e89dee0d27db9fd
Renata Abreu <renata89abreu@gmail.com>
2020-11-02 13:18:46 -0300

    airline entity created and all its data

--- src/main/java/com/accenture/javamos/entity/Airline.java
+++ src/main/java/com/accenture/javamos/entity/Airline.java
@@ -1,2 +1,22 @@
-package com.accenture.javamos.entity;public class Airline {
+package com.accenture.javamos.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "airline")
+@Getter
+public class Airline {
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String logo_url;
 }
